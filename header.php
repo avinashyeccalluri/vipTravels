958ef152a99277f8af150805d8b0900ce98a04ba
avinashyeccalluri <yeccaluriavinash@gmail.com>
2021-01-19 21:23:38 +0530

    header media query adjustments

--- header.php
+++ header.php
@@ -6,26 +6,32 @@
 </head>
 <body>
     <header>
-            <div class="">
-                <a href="tel:+9842371523">9842371523</a> <br/>
-                <a href="tel:+9345301313">9345301313</a>
+        <div class="outer-container">
+            <div class="contact-numbers">
+                <div >
+                    <strong><a class="numbers" href="tel:+9842371523">9842371523</a></strong>
+                </div>
+                <div>
+                    <strong><a class="numbers" href="tel:+9345301313">9345301313</a></strong>
+                </div>
             </div>
-        <div class="container">
-            <div class="logo">
-            <a href="./index.php" class="pt-2">VIP TRAVELS</a>
-            </div>
-            <div class="list">
-                <ul>
-                    <li>Home</li>
-                    <li>Cars</li>
-                    <li>Contact Us</li>
-                    <li>Book car</li>
-                </ul>
-            </div>
-            <div class="hamBurger pt-2">
-                <span></span>
-                <span></span>
-                <span></span>
+            <div class="container">
+                <div class="logo">
+                <a href="./index.php" class="pt-2" style="font-family: 'Luckiest Guy', cursive;">VIP TRAVELS</a>
+                </div>
+                <div class="list">
+                    <ul>
+                        <li>Home</li>
+                        <li>Cars</li>
+                        <li>Contact Us</li>
+                        <li>Book car</li>
+                    </ul>
+                </div>
+                <div class="hamBurger pt-2">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
         </div>
     </header>
